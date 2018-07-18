c5245856835fa87903d54ff9631bb8c9882be863
TKOLM <1946478486@qq.com>
2018-07-18 21:17:11 +0800

    Initial commit

---
+++ src/main/java/com/company/project/configurer/KafkaProducerConfig.java
@@ -0,0 +1,43 @@
+package com.company.project.configurer;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by WZQ on 2018/7/16.
+ */
+@Configuration
+@PropertySource("classpath:kafka.properties")
+public class KafkaProducerConfig {
+    @Value("${kafka.brokers}")
+    private String brokers;
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<String, String>(producerFactory());
+        return kafkaTemplate;
+    }
+
+    public ProducerFactory<String, String> producerFactory() {
+
+        // set the producer properties
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 65536);
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 524288);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<String, String>(properties);
+    }
+}
---
+++ src/main/java/com/company/project/web/UserController.java
@@ -0,0 +1,71 @@
+package com.company.project.web;
+import com.company.project.core.Result;
+import com.company.project.core.ResultGenerator;
+import com.company.project.model.User;
+import com.company.project.service.UserService;
+import com.company.project.utils.RedisUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+* Created by CodeGenerator on 2018/07/07.
+*/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @Autowired
+    private RedisUtil redisUtil;
+    @PostMapping("/add")
+    public Result add(User user) {
+        userService.save(user);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/delete")
+    public Result delete(@RequestParam Integer id) {
+        userService.deleteById(id);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/update")
+    public Result update(User user) {
+        userService.update(user);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/detail")
+    public Result detail(@RequestParam Integer id) {
+        User user = userService.findById(id);
+        return ResultGenerator.genSuccessResult(user);
+    }
+
+    @PostMapping("/list")
+    public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+        PageHelper.startPage(page, size);
+        List<User> list = userService.findAll();
+        PageInfo pageInfo = new PageInfo(list);
+        return ResultGenerator.genSuccessResult(pageInfo);
+    }
+    /*
+    * 整合redis完成
+    * */
+    @PostMapping("/testredis")
+    public Result testredis(){
+        redisUtil.set("name","wzq");
+
+        return ResultGenerator.genSuccessResult(redisUtil.get("name"));
+    }
+
+
+}
